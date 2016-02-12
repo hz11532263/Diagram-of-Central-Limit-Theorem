fc7ea30e13f700408fe870e56f7cc2c75894bd33
hz11532263 <hz11532263@users.noreply.github.com>
2016-02-12 16:55:53 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -9,28 +9,49 @@ shinyUI(fluidPage(
 
   sidebarLayout(
     sidebarPanel(
+      radioButtons("dist", "Distribution type:",
+                   c("Normal" = "norm",
+                     "Uniform" = "unif",
+                     "Log-normal" = "lnorm",
+                     "Exponential" = "exp")
+                  ),
+      br(),
+
       sliderInput( 
                   "bins",  
                   "Number of bins:",  # label
                   min = 1,
                   max = 100,
                   value = 20,
-			step = 1, 
-                  animate=animationOptions(interval=200, loop=FALSE)),  
+		  step = 1, 
+                 ),  
 
       sliderInput( 
-                  "SampleSize", 
-                  "sample size:",  # label
+                  "sampling",  
+                  "Number of sampling:",  # label
+                  min = 2,
+                  max = 500,
+                  value = 100,
+		  step = 2, 
+                  animate=animationOptions(interval=1000/3, loop=FALSE)
+                 ),  
+
+      sliderInput( 
+                  "sampleSize", 
+                  "Sample size:",  # label
                   min = 1,
                   max = 1000,
                   value = 1, 
                   step = 1,
-                  animate=animationOptions(interval=200, loop=FALSE))
+                  animate=animationOptions(interval=1000/3, loop=FALSE)
+                 )
     ),
 
     mainPanel(
-
-      plotOutput("distPlot")
+      tabsetPanel(type = "tabs", 
+        tabPanel("Plot", plotOutput("distPlot")), 
+        tabPanel("Summary", verbatimTextOutput("summary"))
+      )
     )
   )
 ))
